4c5ea93e0952576190df424d5db6a4f0da4872f5
tahafzl123 <taha.1129@outlook.com>
2022-02-02 20:18:04 +0300

    login method created and completed

--- src/main/java/com/shopping/service/UserService.java
+++ src/main/java/com/shopping/service/UserService.java
@@ -1,18 +1,36 @@
 package com.shopping.service;
 
+import java.security.Key;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.shopping.config.JwtService;
 import com.shopping.entity.User;
 import com.shopping.exception.AuthenticationException;
+import com.shopping.model.auth.AuthRequest;
 import com.shopping.model.auth.RegistrationRequest;
 import com.shopping.repository.UserRepository;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class UserService {
 
-	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	private UserRepository userRepository;
+	private JwtService jwtService;
+
+	@Autowired
+	public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+		this.userRepository = userRepository;
+		this.jwtService = jwtService;
+	}
 
 	public Long register(RegistrationRequest request) {
 		Boolean existsEmail = this.userRepository.existsByEmail(request.getEmail());
@@ -23,9 +41,23 @@ public class UserService {
 		user.setName(request.getName());
 		user.setSurname(request.getSurname());
 		user.setUsername(request.getEmail());
-		user.setPassword(request.getPassword());
+		user.setPassword(this.passwordEncoder.encode(request.getPassword()));
 		return this.userRepository.save(user).getId();
 	}
+
+	public String login(AuthRequest request) {
+		User user = this.userRepository.findByEmail(request.getEmail());
+		if(!this.passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+			throw new AuthenticationException("Given email and password not matches");
+		}
+		
+		String token = this.jwtService.createToken(user);
+		
+		
+		
+		return token;
+	}
+	
 	
 	
 	
