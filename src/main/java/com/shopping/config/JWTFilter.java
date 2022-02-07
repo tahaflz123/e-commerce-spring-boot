8bd2b2ac210902d1fad61a6b89f26f251ddd44c5
tahafzl123 <taha.1129@outlook.com>
2022-02-07 21:24:37 +0300

    cleaned

--- src/main/java/com/shopping/config/JWTFilter.java
+++ src/main/java/com/shopping/config/JWTFilter.java
@@ -7,7 +7,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
