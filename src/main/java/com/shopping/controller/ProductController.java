ab1e543b4a53c0174106a304778c2f48a5ea1165
tahafzl123 <taha.1129@outlook.com>
2022-02-08 18:13:33 +0300

    added category filter endpoint

--- src/main/java/com/shopping/controller/ProductController.java
+++ src/main/java/com/shopping/controller/ProductController.java
@@ -7,11 +7,13 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shopping.entity.product.Category;
 import com.shopping.entity.product.Product;
 import com.shopping.model.product.ProductCreateRequest;
 import com.shopping.service.ProductService;
@@ -27,6 +29,11 @@ public class ProductController {
 	@GetMapping
 	public Product product(@PathParam("id") Long id) {
 		return this.productService.getProductById(id);
+	}
+	
+	@GetMapping("/")
+	public List<Product> getAllProducts(){
+		return this.productService.getAllProducts();
 	}
 	
 	@PostMapping("/create")
@@ -48,6 +55,14 @@ public class ProductController {
 	public List<Product> searchProduct(@PathParam("q")String q) {
 		return this.productService.searchProduct(q);
 	}
+	
+	
+	@GetMapping("/category/{category}")
+	public List<Product> getProductsByCategoryAndName(@PathVariable("category") Category category,@PathParam("q") String q){
+		return this.productService.getProductsByCategoryAndName(category,q);
+	}
+	
+	
 	
 	
 }
