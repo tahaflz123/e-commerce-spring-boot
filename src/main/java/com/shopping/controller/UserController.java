eaac682a8cd5af183c5da3af5459eb43b2e01ed9
tahafzl123 <taha.1129@outlook.com>
2022-02-10 22:54:25 +0300

    added updateWallet method

--- src/main/java/com/shopping/controller/UserController.java
+++ src/main/java/com/shopping/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.shopping.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shopping.entity.user.User;
 import com.shopping.model.auth.AuthRequest;
 import com.shopping.model.auth.RegistrationRequest;
 import com.shopping.service.UserService;
@@ -26,5 +30,16 @@ public class UserController {
 	public String login(@RequestBody AuthRequest request) {
 		return this.userService.login(request);
 	}
+	
+	@GetMapping("/profile")
+	public User userProfile() {
+		return this.userService.profile();
+	}
+	
+	@PutMapping("/wallet/upload/{amount}")
+	public Double uploadWallet(@PathVariable("amount") Double amount) {
+		return this.userService.uploadWallet(amount);
+	}
+	
 	
 }
