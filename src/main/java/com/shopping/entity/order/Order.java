3acf15ee8d41f6aa6496c19a543e88002986aac6
tahafzl123 <taha.1129@outlook.com>
2022-02-10 22:52:42 +0300

    added jpa relation annotations and totalcost, amount properties

--- src/main/java/com/shopping/entity/order/Order.java
+++ src/main/java/com/shopping/entity/order/Order.java
@@ -1,12 +1,22 @@
 package com.shopping.entity.order;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.shopping.entity.product.Product;
+import com.shopping.entity.user.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,11 +33,19 @@ public class Order {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 	
-	private Long ordererUserId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	@JsonIgnore
+	private User user;
 	
-	private Long productId;
+	@OneToOne
+	private Product product;
 
 	private Date orderDate;
+	
+	private Integer amount;
+	
+	private Double totalCost;
 	
 	
 }
