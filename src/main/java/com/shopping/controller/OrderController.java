eb3afe5ad204900273f30571b2f604ff35352eec
tahafzl123 <taha.1129@outlook.com>
2022-02-10 22:51:16 +0300

    added deleteOrder endpoint

--- src/main/java/com/shopping/controller/OrderController.java
+++ src/main/java/com/shopping/controller/OrderController.java
@@ -1,14 +1,16 @@
 package com.shopping.controller;
 
+import java.util.List;
+
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.shopping.model.order.OrderResponse;
-import com.shopping.model.order.UserOrdersResponse;
+import com.shopping.entity.order.Order;
 import com.shopping.service.OrderService;
 
 @RestController
@@ -19,19 +21,23 @@ public class OrderController {
 	private OrderService orderService;
 	
 	@GetMapping("/user")
-	private UserOrdersResponse getUserOrders(){
+	private List<Order> getUserOrders(){
 		return this.orderService.getUserOrders();
 	}
 	
 	@GetMapping("/product")
-	private Long orderProduct(@PathParam("id") Long id) {
-		return this.orderService.orderProduct(id);
+	private Long orderProduct(@PathParam("id") Long id,@PathParam("amount") Integer amount) {
+		return this.orderService.orderProduct(id,amount);
 	}
 	
 	@GetMapping("/show")
-	private OrderResponse orderResponse(@PathParam("id")Long id) {
+	private Order orderResponse(@PathParam("id")Long id) {
 		return this.orderService.getOrder(id);
 	}
 	
+	@DeleteMapping("/product/delete")
+	private Boolean deleteOrder(@PathParam("id") Long id) {
+		return this.orderService.deleteOrder(id);
+	}
 	
 }
