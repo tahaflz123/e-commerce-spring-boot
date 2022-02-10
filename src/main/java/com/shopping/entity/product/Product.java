c8efc1fd3ca6462d8caf863d5f0da3947faee710
tahafzl123 <taha.1129@outlook.com>
2022-02-10 22:55:06 +0300

    added stock property

--- src/main/java/com/shopping/entity/product/Product.java
+++ src/main/java/com/shopping/entity/product/Product.java
@@ -1,6 +1,7 @@
 package com.shopping.entity.product;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,7 +10,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.shopping.entity.order.Order;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
